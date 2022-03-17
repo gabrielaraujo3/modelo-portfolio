bf4ff20535ec68c672aa163729a772dab003aa0c
gabrielaraujo3 <araujog388@gmail.com>
2022-03-17 15:29:22 -0300

    :package: formulário de contato

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+/*
+document.querySelector("#nome").addEventListener("change", () =>
+    document.querySelector(".form").classList.add("add"));
+
+document.querySelector("#email").addEventListener("change", () =>
+    document.querySelector(".form").classList.add("add"));
+
+document.querySelector("#mensagem").addEventListener("change", () =>
+    document.querySelector(".form").classList.add("add"));
+    */
